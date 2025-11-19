3d7ce3960613b3645be169ed10eeef0cda6b4081
morgadorodolfo <morgado.rodolfo@gmail.com>
2025-11-19 09:19:51 +0000

    drawGrid and drawHero implemented

--- 20251118-Hero/src/main/kotlin/MainTodo.kt
+++ 20251118-Hero/src/main/kotlin/MainTodo.kt
@@ -41,9 +41,25 @@ private fun updateView(arena: Canvas, pos: Cell) {
  * Draws a grid on the canvas.
  * @receiver The canvas on which to draw the grid.
  */
-//TODO: (1)
+//DONE: (1)
 fun Canvas.drawGrid() {
+    
+    for(x in 0..width step CELL_SIZE)
+        drawLine(x, 0, x, height)
+    for(y in 0..height step CELL_SIZE)
+        drawLine(0, y, width, y)
 
+    //alternative version, using "repeat"
+    /*    
+    repeat(GRID_WIDTH) {
+        val x = it * CELL_SIZE
+        drawLine(x, 0, x, height)
+    }
+    repeat(GRID_HEIGHT) {
+        val y = it * CELL_SIZE
+        drawLine(0, y, width, y)
+    }
+     */
 }
 
 
@@ -52,11 +68,17 @@ fun Canvas.drawGrid() {
  * @receiver The canvas on which to draw the hero.
  * @param p The cell position where the hero should be drawn.
  */
-//TODO: (2)
+//DONE: (2)
 fun Canvas.drawHero(p: Cell) {
 
     val filename = "hero|48,0,48,48"
-    drawImage(...)
+    drawImage(
+        fileName = filename,
+        xLeft = p.col*CELL_SIZE,
+        yTop = p.row*CELL_SIZE,
+        width = CELL_SIZE,
+        height = CELL_SIZE
+    )
 
 }
 
